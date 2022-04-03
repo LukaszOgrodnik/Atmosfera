c9bc63d509e957ecc5b4f2eec9fac8b385a2a53e
LukaszOgrodnik <ogrud2000@gmail.com>
2022-04-03 14:02:09 +0200

    delete old implementation of the testatmosfera.py .

--- testatmosfera.py
+++
@@ -1,9 +0,0 @@
-from unittest import result
-from Atmosfera import Atmosphere 
-#poprawić tę składnię
-Test_atm = Atmosphere(0)
-
-class TestAtmosfera:
-    def test_density(self):
-        result= Test_atm.density
-        assert  result(1.225)
